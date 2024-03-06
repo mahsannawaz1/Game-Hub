c3ce7039a52a7fd8dd75657b8b3669048a4289cd
mahsannawaz1 <nawazehsen@gmail.com>
2024-03-06 22:55:31 +0500

    displaying platform icons

--- game_hub/src/components/GameCard.tsx
+++ game_hub/src/components/GameCard.tsx
@@ -1,17 +1,22 @@
-import React from "react";
 import { Game } from "../hooks/useGames";
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
+import PlatformIconList from "./PlatformIconList";
 
 interface Props {
   game: Game;
 }
 
-const GameCard = ({ game: { id, name, background_image } }: Props) => {
+const GameCard = ({
+  game: { id, name, background_image, parent_platforms },
+}: Props) => {
   return (
     <Card borderRadius={10} overflow="hidden">
       <Image src={background_image} />
       <CardBody>
         <Heading fontSize="2xl">{name}</Heading>
+        <PlatformIconList
+          platforms={parent_platforms.map(({ platform }) => platform)}
+        />
       </CardBody>
     </Card>
   );
--- game_hub/src/hooks/useGames.ts
+++ game_hub/src/hooks/useGames.ts
@@ -2,10 +2,17 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
+export interface Platform{
+    id:number,
+    name:string;
+    slug:string
+}
+
 export interface Game {
     id: number;
     name: string;
     background_image:string
+    parent_platforms:{ platform:Platform }[]
   }
   
   interface FetchGamesResponse {
@@ -16,8 +23,7 @@ export interface Game {
 
 const useGames = ()=>{
 
-    const controller = new AbortController();
-    
+  
     
 
     const [games, setGames] = useState<Game[]>([]);
@@ -25,13 +31,13 @@ const useGames = ()=>{
   
     useEffect(() => {
       apiClient
-        .get<FetchGamesResponse>("/games",{ signal: controller.signal } )
-        .then((res) => setGames(res.data.results))
+        .get<FetchGamesResponse>("/games" )
+        .then((res) => setGames(res.data.results)) 
         .catch((err: Error) => {
             if(err instanceof CanceledError) return;
             setError(err.message);
         });
-        return () => controller.abort();
+       
     }, []);
     return { games,error }
 };
