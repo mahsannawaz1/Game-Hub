14771f74cdd75572410bb55acbe296a94ec25f8f
mahsannawaz1 <nawazehsen@gmail.com>
2024-03-07 23:29:06 +0500

    Get optimized iamges

--- game_hub/src/components/GameCard.tsx
+++ game_hub/src/components/GameCard.tsx
@@ -2,6 +2,7 @@ import { Game } from "../hooks/useGames";
 import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
+import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
   game: Game;
@@ -12,7 +13,7 @@ const GameCard = ({
 }: Props) => {
   return (
     <Card borderRadius={10} overflow="hidden">
-      <Image src={background_image} />
+      <Image src={getCroppedImageUrl(background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{name}</Heading>
         <HStack justifyContent="space-between" alignItems="center">
