66e4e8b5769c5ae872818575058d13aa53d80430
mahsannawaz1 <nawazehsen@gmail.com>
2024-03-09 15:20:26 +0500

    Build Search Input

--- game_hub/src/components/NavBar.tsx
+++ game_hub/src/components/NavBar.tsx
@@ -2,12 +2,13 @@ import { HStack, Image, Text } from "@chakra-ui/react";
 import React from "react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
+import SearchInput from "./SearchInput";
 
 const NavBar = () => {
   return (
-    <HStack justifyContent="space-between" padding={10}>
+    <HStack padding={10}>
       <Image src={logo} boxSize="60px" />
-
+      <SearchInput />
       <ColorModeSwitch />
     </HStack>
   );
